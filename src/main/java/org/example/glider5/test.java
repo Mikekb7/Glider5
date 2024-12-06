7e6e908c9f458adab478096b86b3dd63250bbdcf
mekht <michaelbonger831@gmail.com>
2024-12-06 01:42:34 -0500

    checking

--- src/main/java/org/example/glider5/test.java
+++ src/main/java/org/example/glider5/test.java
@@ -2,6 +2,6 @@ package org.example.glider5;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello, World yayyy!");
+        System.out.println("Hello, World yayyy hiphipp jj!");
     }
 }
