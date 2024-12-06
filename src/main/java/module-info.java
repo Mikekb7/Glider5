10bef5cec4f03c047743bf989f3dc7524eb3eb8b
Michael Bonger <michaelbonger831@gmail.com>
2024-12-06 00:35:23 -0500

    Database Connection established

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module org.example.glider5 {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens org.example.glider5 to javafx.fxml;
     exports org.example.glider5;
